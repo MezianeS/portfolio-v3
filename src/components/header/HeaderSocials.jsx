e442b22022e581d52afce0d578687e9fde74eaba
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-12 21:27:24 +0200

    Add socials div

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,13 @@ import { VscGithub } from 'react-icons/vsc'
 const HeaderSocials = () => {
   return (
     <div className='header__socials--menu'>
-      <TiSocialLinkedinCircular />
-      <VscGithub />
+      <div className="header__socials--icons">
+        <TiSocialLinkedinCircular />
+        <VscGithub />
+      </div>
+      <div className="header__socials--contactme">
+        <a href="#">Contact me</a>
+      </div>
     </div>
   )
 }
