8c6a69bb259e7a2b4f4595bd983ab6067663df4e
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-18 12:38:08 +0200

    Fix repo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,20 @@ const Header = () => {
       <div className="header__moiastronaute">
         <img src={require("../../assets/sofiane.png")}/>
       </div>
+      <div className="container header__title">
+        <h1>Portfolio</h1>
+        {/* <input id="menu-toggle" type="checkbox" />
+        <label className="menu-button-container" for="menu-toggle">
+          <div className="menu-button"></div>
+        </label> */}
+        <ul className="header__menu">
+          <li><a href="#Skills">Skills</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#Services">Services</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+      </div>
+
     </header>
   )
 }
