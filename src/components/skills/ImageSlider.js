2634904e4fe2d9862d0d75ac4b5eb11e20abce3d
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-25 12:27:21 +0200

    Update

--- src/components/skills/ImageSlider.js
+++ src/components/skills/ImageSlider.js
@@ -5,13 +5,26 @@ const ImageSlider = ({slides}) => {
     const [currentIndex, setCurrentUser] = useState(0);
 
     const slideStyles = {
+        width: "100%",
+        height: "100%",
         borderRadius: "10px",
         backgroundPosition:'center',
         backgroundRepeat: 'no-repeat',
-        backgroundImage: `url(${slides[currentIndex].url})`,        
+        backgroundImage: `url(${slides[currentIndex].url})`,  
+              
+    };
+    const leftArrowStyle = {
+        position: 'absolute',
+        top: '50%',
+    };
+    const rightArrowStyle = {
+        position: 'absolute',
+        top: '50%',
     }
     return (
         <div className="slider__menu--sliderstyle">
+            <div style={leftArrowStyle} >(</div>
+            <div style={rightArrowStyle}>)</div>
             <div style={slideStyles} className="slider__menu--slide">
             </div>
             
