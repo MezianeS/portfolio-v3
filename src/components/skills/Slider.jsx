c51a3a3945868ec6ec90bdef55ead323929f0eb9
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-26 12:34:23 +0200

    Update Slider.jsx

--- src/components/skills/Slider.jsx
+++ src/components/skills/Slider.jsx
@@ -8,6 +8,7 @@ const Slider = () => {
         {url: "http://localhost:3000/static/media/radialbarquatro.0fb93d427223acc71e2c86cedd8fe2a8.svg", title: 'Frontend Development'},
         {url: "http://localhost:3000/static/media/radialbarsept.a4e61ab30fa3dccc12952265f48f19f2.svg", title: 'Backend Development'},
         {url: "http://localhost:3000/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'UI Design'},
+        {url: "http://localhost:3000/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'Brand Identity'},
     ];
 
   return (
