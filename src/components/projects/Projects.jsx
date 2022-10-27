c85ffcc0437a69977424e618c503b3b695ff1118
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-27 22:10:28 +0200

    Create projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,27 @@ import './projects.css'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects__container'>
+      <div className="container projects__container">
+        <div className='projects__entete'>
+          <h2 className='projects__entete--title'>Projects</h2>
+          <p className='projects__entete--text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos facere tempora expedita animi repudiandae optio architecto aut illo accusantium natus unde porro non aperiam eligendi, sequi nesciunt dolor deleniti qui!</p>
+        </div>
+        <div className='projects__menu'>
+          <div className="projects__menu--button">
+            <div className="nav-item">
+              <a href="#">Tab1</a>
+            </div>
+            <div className="nav-item">
+              <a href="#">Tab2</a>
+            </div>
+            <div className="nav-item">
+              <a href="#">Tab3</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
