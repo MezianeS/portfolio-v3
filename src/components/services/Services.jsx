fc87032d2f56f83e8cba8204cbb7ab80530d5882
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-29 23:20:08 +0200

    Create services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,16 @@ import './service.css'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='container services__container'>
+      <div className="services__entete">
+        <h2 id="Services" className="services__entete--title">
+          Services
+        </h2>
+        <p className="services__entete--text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ipsam harum necessitatibus praesentium reiciendis non quibusdam illo?
+        </p>
+      </div>
+    </div>
   )
 }
 
