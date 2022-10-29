258627a4942d7c56f79d7df586ea2fc39915c083
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 01:08:50 +0200

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,13 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact__background'>
+      <div className="container contact__container">
+        <div className='contact__entete'>
+          <h2 id='Contact'>Contact</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
