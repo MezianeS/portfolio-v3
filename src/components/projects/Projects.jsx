d5a04dc6efa57268bb0227b6369169a28baffbba
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-29 22:03:14 +0200

    Update css

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,6 +9,26 @@ import Whalewebsite from './../../assets/projects/website-6.png'
 
 
 const Projects = () => {
+  function FadeInSection(props) {
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+      return () => observer.unobserve(domRef.current);
+    }, []);
+    return (
+      <div
+        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
   return (
     <div className='projects__container'>
       <div className="container projects__container">
@@ -29,62 +49,64 @@ const Projects = () => {
             </div> */}
           </div>
         </div>
-      <div className="projects__menu--projectslist">
-        <div className='projects-item'>
-          <a href="https://www.whaleverse.cc/">
-            <img src={ Whalewebsite } alt="Whaleverse.cc" />
-            <div className="projects-txt">
-              <h4>NFT Showcase</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
+      <FadeInSection>
+        <div className="projects__menu--projectslist">
+          <div className='projects-item'>
+            <a href="https://www.whaleverse.cc/">
+              <img src={ Whalewebsite } alt="Whaleverse.cc" />
+              <div className="projects-txt">
+                <h4>NFT Showcase</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
+          <div className='projects-item'>
+            <a href="https://hongkong.mezianesofiane.com/">
+              <img src={ HKwebsite } alt="Photo de HongKong" />
+              <div className="projects-txt">
+                <h4>Hong Kong: photoweb</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
+          <div className='projects-item'>
+            <a href="https://alaskaproject.mezianesofiane.com/">
+              <img src={ Ecrivwebsite } alt="Site pour un écrivain" />
+              <div className="projects-txt">
+                <h4>Alaska essay</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
+          <div className='projects-item'>
+            <a href="https://www.bikessels.mezianesofiane.com/">
+              <img src={ Bikessels } alt="Site de location de vélo" />
+              <div className="projects-txt">
+                <h4>Bicycle rental app</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
+          <div className='projects-item'>
+            <a href="https://agence-strasbourg.mezianesofiane.com/">
+              <img src={ WordpressStrass } alt="Tourisme Strasbourg" />
+              <div className="projects-txt">
+                <h4>Strasbourg tourist office</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
+          <div className='projects-item'>
+            <a href="https://webagency.mezianesofiane.com/">
+              <img src={ Webagencywebsite } alt="Web agency" />
+              <div className="projects-txt">
+                <h4>Web Integration</h4>
+                <span>Design & Development</span>
+              </div>
+            </a>
+          </div>
         </div>
-        <div className='projects-item'>
-          <a href="https://hongkong.mezianesofiane.com/">
-            <img src={ HKwebsite } alt="Photo de HongKong" />
-            <div className="projects-txt">
-              <h4>Hong Kong: photoweb</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
-        </div>
-        <div className='projects-item'>
-          <a href="https://alaskaproject.mezianesofiane.com/">
-            <img src={ Ecrivwebsite } alt="Site pour un écrivain" />
-            <div className="projects-txt">
-              <h4>Alaska essay</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
-        </div>
-        <div className='projects-item'>
-          <a href="https://www.bikessels.mezianesofiane.com/">
-            <img src={ Bikessels } alt="Site de location de vélo" />
-            <div className="projects-txt">
-              <h4>Bicycle rental app</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
-        </div>
-        <div className='projects-item'>
-          <a href="https://agence-strasbourg.mezianesofiane.com/">
-            <img src={ WordpressStrass } alt="Tourisme Strasbourg" />
-            <div className="projects-txt">
-              <h4>Strasbourg tourist office</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
-        </div>
-        <div className='projects-item'>
-          <a href="https://webagency.mezianesofiane.com/">
-            <img src={ Webagencywebsite } alt="Web agency" />
-            <div className="projects-txt">
-              <h4>Web Integration</h4>
-              <span>Design & Development</span>
-            </div>
-          </a>
-        </div>
-      </div>
+      </FadeInSection>
       </div>
     </div>
   )
