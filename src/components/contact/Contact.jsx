8a29f242c3e9946666f10e24bc35cf3c29595f21
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 01:34:57 +0200

    Install emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import './contact.css'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sslu73i', 'template_nbhui15', form.current, '-Q7llD49klfap9tVT')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
   return (
     <div className='contact__background'>
       <div className="container contact__container">
@@ -9,6 +25,14 @@ const Contact = () => {
           <h2 id='Contact' className='contact__entete--title'>Contact</h2>
           <p className="contact__entete--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         </div>
+
+        {/* mail */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form> 
       </div>
     </div>
   )
