99b222cee0e954fe14b512d9155086a3ac825d99
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 17:04:42 +0100

    Update

--- src/components/header/HeaderPresentation.jsx
+++ src/components/header/HeaderPresentation.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import './headerpresentation.css'
 
 
 const HeaderPresentation = () => {
   return (
-    <div>
-      Headersocials
-    </div>
+    <section className='container headerpresentation__blck'>
+      <div className=''>
+        <h3>Welcome to my Portfolio</h3>
+      </div>
+    </section>
   )
 }
 
