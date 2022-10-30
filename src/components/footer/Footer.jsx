1b8a724c0f92df01772fadeb0c940e9e9a32645e
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 15:43:21 +0100

    Update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,13 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer__background'> 
+      <div className="container footer__container">
+        <div className="footer__title">
+          <h4>Portfolio</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
