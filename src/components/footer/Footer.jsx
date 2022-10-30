6f44d3b662f7559de6064bad13f9d2d99a066c19
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 16:51:39 +0100

    Update Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,19 +11,21 @@ const Footer = () => {
           <div className="footer__title">
             <h4>Portfolio</h4>
           </div>
-          <div className="footer__icons">
-            <ul>
-              <li>
-                <a href="#" className='footer__icons--img'><BsGithub className='footer__icons--params'/></a>
-              </li>
-              <li>
-                <a href="#" className='footer__icons--img'><FaLinkedinIn  className='footer__icons--params'/>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className='footer__copyright'>
-            <p>Copyright2022: All Rights Reserved</p>
+          <div className='footer__icons--copy'>
+            <div className="footer__icons">
+              <ul>
+                <li>
+                  <a href="#" className='footer__icons--img'><BsGithub className='footer__icons--params'/></a>
+                </li>
+                <li>
+                  <a href="#" className='footer__icons--img'><FaLinkedinIn  className='footer__icons--params'/>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='footer__copyright'>
+              <p>Copyright2022: All Rights Reserved</p>
+            </div>
           </div>
         </div>
       </div>
