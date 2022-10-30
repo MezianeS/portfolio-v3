d79c779079b2c599173205bae26990f3150409dc
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 22:25:53 +0100

    Update Slider.jsx

--- src/components/skills/Slider.jsx
+++ src/components/skills/Slider.jsx
@@ -5,10 +5,10 @@ import './slider.css'
 
 const Slider = () => {
     const slides =[
-        {url: "http://localhost:3000/static/media/radialbarquatro.0fb93d427223acc71e2c86cedd8fe2a8.svg", title: 'Frontend Development'},
-        {url: "http://localhost:3000/static/media/radialbarsept.a4e61ab30fa3dccc12952265f48f19f2.svg", title: 'Backend Development'},
-        {url: "http://localhost:3000/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'UI Design'},
-        {url: "http://localhost:3000/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'Brand Identity'},
+        {url: "https://www.mezianesofiane.com/static/media/radialbarquatro.0fb93d427223acc71e2c86cedd8fe2a8.svg", title: 'Frontend Development'},
+        {url: "https://www.mezianesofiane.com/static/media/radialbarsept.a4e61ab30fa3dccc12952265f48f19f2.svg", title: 'Backend Development'},
+        {url: "https://www.mezianesofiane.com/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'UI Design'},
+        {url: "https://www.mezianesofiane.com/static/media/radialbarquatrovingt.877b63f38bb525e26420137781d95170.svg", title: 'Brand Identity'},
     ];
 
   return (
