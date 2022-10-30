bdd911d02840a239d3d3d67d6af37e68d10711fd
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 15:49:03 +0100

    Update bottom

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './footer.css'
+import { BsGithub } from 'react-icons/bs'
+import { FaLinkedinIn } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -8,6 +10,18 @@ const Footer = () => {
         <div className="footer__title">
           <h4>Portfolio</h4>
         </div>
+        <div className="footer__icons">
+          <ul>
+            <li>
+              <a href="#" className='header__icon'><BsGithub className='header__icon--params'/></a>
+            </li>
+            <li>
+              <a href="#" className='header__icon'><FaLinkedinIn  className='header__icon--params'/>
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   )
