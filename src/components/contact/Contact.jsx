47d4b8bc73b54e530428ab6dd02d7d71fe50da44
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-10-30 15:03:41 +0100

    Up contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 import './contact.css'
+import Contactimg from './../../assets/form-img.png'
 
 const Contact = () => {
   const form = useRef();
@@ -21,20 +22,24 @@ const Contact = () => {
   return (
     <div className='contact__background'>
       <div className="container contact__container">
-        <div className='contact__entete'>
-          <h2 id='Contact' className='contact__entete--title'>Contact</h2>
-          <p className="contact__entete--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <div className="contact__main--img">
+          <img src={ Contactimg } alt="Développeur devant son pc" className='contact__main--png'/>
         </div>
-        {/* mail */}
-        <div className='contact__main--mail'>
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name="name" placeholder='Full Name' required/>
-            <input type="email" name="email" placeholder='Email address' required />
-            <textarea name="message" rows="7" placeholder='Message' required ></textarea>
-            <button type="submit" className='btn btn-primary'>Send Message</button>
-          </form> 
+        <div className="contact__menu">
+          <div className='contact__entete'>
+            <h2 id='Contact' className='contact__entete--title'>Get In Touch</h2>
+          </div>
+          {/* mail */}
+          <div className='contact__main--mail'>
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder='Name' required/>
+              <input type="email" name="email" placeholder='Email address' required />
+              <textarea name="message" rows="7" placeholder='Message' required ></textarea>
+              <button type="submit" className='contact__main--btn'>Send</button>
+            </form> 
+          </div>
+          {/* end mail */}
         </div>
-        {/* end mail */}
       </div>
     </div>
   )
